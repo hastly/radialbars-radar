6658ae8eb317dba6ca87afd454089453e7340cdf
Tova Kowalt <tovakovalt@gmail.com>
2018-09-10 09:30:53 +0300

    d3js basic chart test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import BarChart from './BarChart'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h2>Basic D3-React test</h2>
+        </div>
+        <div>
+          <BarChart data={[1, 3, 5, 17]} size={[500, 500]} />
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
